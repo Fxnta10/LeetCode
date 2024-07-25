9c3ac3898ad32f31cd472b547575ea9194a7824f
Surya <115891364+Fxnta10@users.noreply.github.com>
2024-07-25 15:57:19 +0530

    Time: 60 ms (21.62%), Space: 11 MB (24.41%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
+        //find the where u can find abc from the start 
+        //add all the subarrays u can get by increasing the right value 
+        //now contract the left pointer till a point where u cant find abc in that array
         unordered_map<char,int>mp;
         int left=0;
         int ans=0;
